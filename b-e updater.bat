45ee2bf270a1be0355f824cbde7793bbcace2b13
Adam K <adam-kay-@outlook.com>
2023-12-11 22:52:26 +0000

    Overhauled `b-e updater` to match changes in `batch encoder`

--- b-e updater.bat
+++ b-e updater.bat
@@ -38,23 +38,28 @@ setlocal enabledelayedexpansion
 :Update
 	cls
 	echo Downloading information...
-	curl --silent -o batch_update.txt https://gist.githubusercontent.com/Adam-Kay/ec5da0ff40e8eb14beee2242161f5191/raw
-	for /f "tokens=1,2,3 delims=|" %%A in (batch_update.txt) do (
-		set UpdateVersion=%%A
-		set UpdateAPIURL=%%B
-		set UpdateBrowserURL=%%C
-	)
+	set "updateFileName=batch_update.json"
+	curl --silent -L -H "Accept: application/vnd.github+json" -o %updateFileName% https://api.github.com/repos/Adam-Kay/Batch-Encoder/releases/latest
+	rem curl --silent -o batch_update.txt https://gist.githubusercontent.com/Adam-Kay/ec5da0ff40e8eb14beee2242161f5191/raw
+	
+	>%TEMP%\batch_update.tmp findstr "tag_name" %updateFileName%
+	<%TEMP%\batch_update.tmp set /p "ver_entry="
+	set "ver=%ver_entry:~15,-2%"
+	set "UpdateVersion=%ver:~1%"
+	
+	set regex_command=powershell -Command "$x = Get-Content %updateFileName% -Raw; $k = $x | Select-String -Pattern '(?s)url(((?^^^!url).)*?)batch\.encoder'; $g = $k.Matches.Value | Select-String -Pattern '^""[^^^^"""]+?^""",'; $g.Matches.Value"
+	
+	FOR /F "tokens=*" %%g IN ('%regex_command%') do (SET API_link_entry=%%g)
+	set "UpdateAPIURL=%API_link_entry:~1,-2%"
 
-	rem Test if any of them are blank
-	for %%a in (UpdateVersion, UpdateAPIURL, UpdateBrowserURL) do if not defined %%a goto AutoUpdateError
-
-
+	for %%a in (ver_entry, API_link_entry, UpdateVersion, UpdateAPIURL) do if not defined %%a goto AutoUpdateError
+	
 	echo Version found^^! ^(%UpdateVersion%^)
 	echo Proceeding with force update in 5 seconds, press CTRL+C or close window to cancel.
 	echo.
 	timeout /nobreak /t 5 > nul
 	echo Downloading files...
-	curl --silent -L -H "Accept: application/octet-stream" -o "batch encoder %UpdateVersion%.bat" %UpdateAPIURL%
+	curl --silent -L -H "Accept: application/octet-stream" -o "batch encoder v%UpdateVersion%.bat" %UpdateAPIURL%
 	echo.
 	echo Download complete. The program will now clean up and restart.
 	pause
@@ -63,13 +68,18 @@ setlocal enabledelayedexpansion
 			del "%BEFile%"
 		)
 	)
-	del "batch_update.txt"
-	(goto) 2>nul & "batch encoder %UpdateVersion%.bat" --updated-from "%~f0"
+	del "%updateFileName%"
+	(goto) 2>nul & "batch encoder v%UpdateVersion%.bat" --updated-from "%~f0"
 	
 :AutoUpdateError
 	del "%updateFileName%"
 	echo.
-	echo There was a problem with the auto-updater. Exiting...
+	echo.
+	echo *******************************************************
+	echo There was a problem with the auto-updater. You can download the latest version of the program at: 
+	echo https://github.com/Adam-Kay/Batch-Encoder/releases
+	echo.
+	echo Exiting...
 	echo.
 	pause
 	exit
