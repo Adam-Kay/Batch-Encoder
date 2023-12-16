79ce08d4d247a309ae21ecd9ffcc7167d1178848
Adam K <adam-kay-@outlook.com>
2023-12-16 20:18:29 +0000

    Added readable variables for common color formatting

--- batch encoder.bat
+++ batch encoder.bat
@@ -13,27 +13,29 @@ if /i "%1"=="--updated-from" (
 set "icongray=[7;90m"
 set "iconyellow=[7;33m"
 set "icongreen=[7;32m"
-set "iconend=[0m"
-
+set "textgray=[90m"
+set "textgreen=[32m"
+set "textred=[31m"
+set "formatend=[0m"
 
 :AskProceed
 	call:ClearAndTitle
-	echo %icongray% i %iconend% This program will aim to encode all .mp4 files in the folder it's placed in and delete the originals.
-	set /p "startconfirmation=Do you want to proceed? [90m[Y/N][0m: "
+	echo %icongray% i %formatend% This program will aim to encode all .mp4 files in the folder it's placed in and delete the originals.
+	set /p "startconfirmation=Do you want to proceed? %textgray%[Y/N]%formatend%: "
 	if /i "%startconfirmation%"=="n" exit
 	if /i "%startconfirmation%"=="y" (goto AskUpdate)
 	goto AskProceed
 
 :AskUpdate
 	call:ClearAndTitle
-	set /p "updateconfirmation=%icongray% ^ %iconend% Would you like to check for an update? [90m[Y/N][0m: "
+	set /p "updateconfirmation=%icongray% ^ %formatend% Would you like to check for an update? %textgray%[Y/N]%formatend%: "
 	if /i "%updateconfirmation%"=="n" (goto FFMPEGLocation)
 	if /i "%updateconfirmation%"=="y" (goto AutoUpdate)
 	goto AskUpdate
 	
 :AutoUpdate
 	call:ClearAndTitle
-	echo %icongray% i %iconend% Downloading information...
+	echo %icongray% i %formatend% Downloading information...
 	set "updateFileName=batch_update.json"
 	curl --silent -L -H "Accept: application/vnd.github+json" -o %updateFileName% https://api.github.com/repos/Adam-Kay/Batch-Encoder/releases/latest
 	rem curl --silent -o batch_update.txt https://gist.githubusercontent.com/Adam-Kay/ec5da0ff40e8eb14beee2242161f5191/raw
@@ -54,21 +56,21 @@ set "iconend=[0m"
 
 	echo.
 	if /i "%UpdateVersion%"=="%CurrentVersion%" (
-		echo %icongray% - %iconend% Current version is up-to-date.
+		echo %icongray% - %formatend% Current version is up-to-date.
 		echo.
 		echo The program will now restart.
 		call:GrayPause
 		del "%updateFileName%"
 		goto AskProceed
 	) else (
-		echo %iconyellow% ^^! %iconend% Differing version found^^! ^([31m%CurrentVersion%[0m -^> [32m%UpdateVersion%[0m^)
+		echo %iconyellow% ^^! %formatend% Differing version found^^! ^(%textred%%CurrentVersion%%formatend% -^> %textgreen%%UpdateVersion%%formatend%^)
 		echo Proceeding with update in 5 seconds, press CTRL+C or close window to cancel.
 		echo.
 		timeout /nobreak /t 5 > nul
 		echo Downloading files...
 		curl --silent -L -H "Accept: application/octet-stream" -o "batch encoder %UpdateVersion%%append%.bat" %UpdateAPIURL%
 		echo.
-		echo %icongreen% i %iconend% Download complete. The program will now clean up and restart. 
+		echo %icongreen% i %formatend% Download complete. The program will now clean up and restart. 
 		call:GrayPause
 		del "%updateFileName%"
 		(goto) 2>nul & "batch encoder %UpdateVersion%%append%.bat" --updated-from "%~f0"
@@ -77,7 +79,7 @@ set "iconend=[0m"
 :FFMPEGLocation
 	call:ClearAndTitle
 	rem TODO: detect FFMPEG if in same folder
-	set /p "LOCATION=%icongray% ? %iconend% Where is FFMPEG.exe located? (paste full path): "
+	set /p "LOCATION=%icongray% ? %formatend% Where is FFMPEG.exe located? (paste full path): "
 	
 set /a "COUNTER=-1" 
 
@@ -102,7 +104,7 @@ set "INPUTFILE="
 			call:GrayTimeout 2
 		) else (
 			set /a "COUNTER+=1" 
-			echo [100m Encoding !COUNTER! of %TOTAL% [0m
+			echo [100m Encoding !COUNTER! of %TOTAL% %formatend%
 			rem echo **********************************
 				
 			set "TESTSTRING=!INPUTFILE:~-4!"
@@ -129,23 +131,23 @@ set "INPUTFILE="
 				echo ***********************
 				echo.
 				
-				echo %icongray% ^| %iconend% Checking for output file...
+				echo %icongray% ^| %formatend% Checking for output file...
 				if /i not exist "%CD%\!OUTPUTFILE!" goto CritError
 				echo - Output file exists^^!
 				echo.
 				
-				echo %icongray% ^| %iconend% Checking output file length...
+				echo %icongray% ^| %formatend% Checking output file length...
 				for /F "tokens=*" %%g in (
 					'powershell -Command "$Shell = New-Object -ComObject Shell.Application; $Folder = $Shell.Namespace('%cd%'); $Folder.GetDetailsOf($Folder.ParseName('!INPUTFILE!'), 27)"'
-					) do (set LEN_inP=%%g)
+					) do (set LEN_INP=%%g)
 				for /F "tokens=*" %%g in (
 					'powershell -Command "$Shell = New-Object -ComObject Shell.Application; $Folder = $Shell.Namespace('%cd%'); $Folder.GetDetailsOf($Folder.ParseName('!OUTPUTFILE!'), 27)"'
 					) do (set LEN_OUT=%%g)
-				echo Input file: !LEN_inP! - Output file: !LEN_OUT!
-				if /i not "!LEN_inP!"=="!LEN_OUT!" goto CritError
+				echo Input file: !LEN_INP! - Output file: !LEN_OUT!
+				if /i not "!LEN_INP!"=="!LEN_OUT!" goto CritError
 				echo - File lengths match^^!
 				echo.
-				echo %icongreen% ^| %iconend% Safely proceeding with input file recycling...
+				echo %icongreen% ^| %formatend% Safely proceeding with input file recycling...
 				timeout /nobreak /t 1 > nul
 				
 				powershell -Command "(Get-Item '%CD%\!OUTPUTFILE!').CreationTime=((Get-Item '%CD%\!INPUTFILE!').CreationTime)"
@@ -159,7 +161,7 @@ set "INPUTFILE="
 	)
 	
 call:ClearAndTitle
-echo [42;97m Completed encoding %TOTAL% files. [0m
+echo [42;97m Completed encoding %TOTAL% files. %formatend%
 
 :EndPause
 	call:GrayPause
@@ -186,30 +188,30 @@ echo [42;97m Completed encoding %TOTAL% files. [0m
 	goto AskProceed
 	
 :GrayPause
-	echo [90m
+	echo %textgray%
 	pause
-	echo [0m
+	echo %formatend%
 	goto:eof
 	
 :GrayTimeout
 	set timer=%~1
-	<nul set /p=[90m
+	<nul set /p=%textgray%
 	for %%a in (timer) do if not defined %%a (
 		timeout /t 5
 	) else (
 		timeout /t %timer%
 	)
-	echo [0m
+	echo %formatend%
 	goto:eof
 	
 :ErrorLine
-	rem echo [4;31m                                                            [0m
-	echo [31m************************************************************[0m
-	rem echo [7;31m ********************************************************** [0m
+	rem echo [4;31m                                                            %formatend%
+	echo %textred%************************************************************%formatend%
+	rem echo [7;31m ********************************************************** %formatend%
 	goto:eof
 
 :ClearAndTitle
 	cls
-	echo [7m Batch Encoder %CurrentVersion% [0m
+	echo [7m Batch Encoder %CurrentVersion% %formatend%
 	echo.
 	goto:eof
