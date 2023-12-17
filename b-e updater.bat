5d8964bd04c5594d3c88134ddb655ae0ad7320c3
Adam K <adam-kay-@outlook.com>
2023-12-17 23:44:16 +0000

    Added readable color formatting to match with 79ce08d

--- b-e updater.bat
+++ b-e updater.bat
@@ -4,12 +4,15 @@ setlocal enabledelayedexpansion
 set "icongray=[7;90m"
 set "iconyellow=[7;33m"
 set "icongreen=[7;32m"
-set "iconend=[0m"
+set "textgray=[90m"
+set "textgreen=[32m"
+set "textred=[31m"
+set "formatend=[0m"
 
 
 :AskProceed
 	cls
-	SET /p "ProceedConf=%icongray% i %iconend% This program will attempt to forcibly update the batch encoder script. Do you want to proceed? [90m[Y/N][0m: "
+	SET /p "ProceedConf=%icongray% i %formatend% This program will attempt to forcibly update the batch encoder script. Do you want to proceed? %textgray%[Y/N]%formatend%: "
 	IF /i "%ProceedConf%"=="n" exit
 	IF /i "%ProceedConf%"=="y" (goto AskFile)
 	goto AskProceed
@@ -19,7 +22,7 @@ set "iconend=[0m"
 	cls
 	set CONFIRMATION=
 	for %%f in (*batch*encode*.bat) do (
-		SET /p "FileConf=%icongray% ? %iconend% Is this the current batch encoder script file?: '%%f' [90m[Y/N][0m: "
+		SET /p "FileConf=%icongray% ? %formatend% Is this the current batch encoder script file?: '%%f' %textgray%[Y/N]%formatend%: "
 		IF /i "!FileConf!"=="y" (
 			set BEFile=%%f
 			goto Update
@@ -35,7 +38,7 @@ set "iconend=[0m"
 	)
 	
 :AskAnyway
-	SET /p "AnywayConf=%iconyellow% X %iconend% Unable to find batch encoder script. Would you like to download the latest file anyway? [90m[Y/N][0m: "
+	SET /p "AnywayConf=%iconyellow% X %formatend% Unable to find batch encoder script. Would you like to download the latest file anyway? %textgray%[Y/N]%formatend%: "
 	IF /i "%AnywayConf%"=="n" exit
 	IF /i "%AnywayConf%"=="y" (goto Update)
 	goto AskAnyway
@@ -43,7 +46,7 @@ set "iconend=[0m"
 
 :Update
 	cls
-	echo %icongray% i %iconend% Downloading information...
+	echo %icongray% i %formatend% Downloading information...
 	set "updateFileName=batch_update.json"
 	curl --silent -L -H "Accept: application/vnd.github+json" -o %updateFileName% https://api.github.com/repos/Adam-Kay/Batch-Encoder/releases/latest
 	rem curl --silent -o batch_update.txt https://gist.githubusercontent.com/Adam-Kay/ec5da0ff40e8eb14beee2242161f5191/raw
@@ -61,14 +64,14 @@ set "iconend=[0m"
 	for %%a in (ver_entry, API_link_entry, UpdateVersion, UpdateAPIURL) do if not defined %%a goto AutoUpdateError
 	
 	echo.
-	echo %iconyellow% ^^! %iconend% Version found^^! ^([32m%UpdateVersion%^[0m)
+	echo %iconyellow% ^^! %formatend% Version found^^! ^(%textgreen%%UpdateVersion%^%formatend%)
 	echo Proceeding with force update in 5 seconds; close window to cancel.
 	echo.
 	timeout /nobreak /t 5 > nul
 	echo Downloading files...
 	curl --silent -L -H "Accept: application/octet-stream" -o "batch encoder %UpdateVersion%.bat" %UpdateAPIURL%
 	echo.
-	echo %icongreen% i %iconend% Download complete. The program will now clean up and restart.
+	echo %icongreen% i %formatend% Download complete. The program will now clean up and restart.
 	call:GrayPause
 	if defined BEFile (
 		if not "%BEFile%"=="batch encoder %UpdateVersion%.bat" (
@@ -82,7 +85,7 @@ set "iconend=[0m"
 	del "%updateFileName%"
 	echo.
 	echo.
-	echo [31m************************************************************[0m
+	echo %textred%************************************************************%formatend%
 	echo.
 	echo There was a problem with the auto-updater. You can download the latest version of the program at: 
 	echo https://github.com/Adam-Kay/Batch-Encoder/releases
@@ -92,7 +95,7 @@ set "iconend=[0m"
 	exit
 	
 :GrayPause
-	echo [90m
+	echo %textgray%
 	pause
-	echo [0m
+	echo %formatend%
 	goto:eof
